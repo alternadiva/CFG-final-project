b66db000d1f35ef4aa24a57dfd8281e1c02f03e5
alternadiva <85565743+alternadiva@users.noreply.github.com>
2021-11-04 18:25:51 +0100

    home + landing pages

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function openMenu() {
+    document.getElementById("menu").style.height = "100%";
+  }
+  
+function closeMenu() {
+    document.getElementById("menu").style.height = "0%";
+  }
+
+const openButton = document.getElementById("open-btn");
+const closeButton = document.getElementById("close-btn");
+
+openButton.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
